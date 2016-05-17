04baf57b1f6ed1404bb7567daab29ac99bbc01e1
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2016-05-17 13:54:47 -0500

    Update Resp_XNAT.sh

--- Resp_XNAT.sh
+++ Resp_XNAT.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # scrip to start and stop tomcat in the Xnat server
 # then the database backup 
-echo "Raiz&Dharma" | sudo -S service tomcat7 stop
+echo "**** ******* ******************** ********** ****"
+echo "XNAT-DB databese backup on data"
+echo "pgDump STARTED on" `date`
+
+echo "Raiz&Dharma" | /usr/bin/sudo -S service tomcat7 stop
 
 pass="xnat01"
 export PGPASSWORD="$pass"
-pg_dump -v -i -U xnat01 -Fc xnat > /data/XNAT-DB/backup/xnat_$(date +%m%d%y).dump
+/usr/bin/pg_dump -v -i -U xnat01 -Fc xnat > /data/XNAT-DB/backup/xnat_$(date +%m%d%y).dump
 
-echo "Raiz&Dharma" | sudo -S service tomcat7 start
+echo "Raiz&Dharma" | /usr/bin/sudo -S service tomcat7 start
+
 # End of the database backup
+echo "*************************************************"
+echo "XNAT-DB database backup on data"
+echo "pgDump ENDED on" `date`
